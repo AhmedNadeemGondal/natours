bed2da2938115260d59eccf9b1defb2463499efc
ahmed.gondal <ahmednadeemgondal92@gmail.com>
2025-02-28 11:45:43 +0500

    Implemente payment alert

--- public/js/index.js
+++ public/js/index.js
@@ -73,4 +73,7 @@ if (bookBtn)
     bookTour(tourId);
   });
 
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 20);
+
 // console.log('ssss');
